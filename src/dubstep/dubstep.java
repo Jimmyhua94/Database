fbb197fe0376ce247a25a4bfc660c3e53ffe9a37
Jimmyhua94 <jimmyhua@buffalo.edu>
2016-02-05 23:34:47 -0500

    Main.java

--- src/dubstep/dubstep.java
+++
@@ -1,7 +0,0 @@
-package dubstep;
-
-public class dubstep {
-	public static void Main(String[] args){
-	System.out.println("We, the members of our team, agree that we will not submit any code that we have not written ourselves, share our code with anyone outside of our group, or use code that we have not written ourselves as a reference.");
-	}
-}
